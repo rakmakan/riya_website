eb3969b83301d967b893a7f6346c1606c6d1fd06
Rakshit Makan <rakshitmakan.dev@gmail.com>
2025-05-27 00:10:10 -0400

    Refactor code structure for improved readability and maintainability

--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,9 @@
 <header class="header fixed-top">
     <nav class="navbar navbar-expand-lg navbar-light bg-white">
         <div class="container">
-            <a class="navbar-brand" href="<?php echo strpos($_SERVER['PHP_SELF'], 'views/') !== false ? '../index.php' : 'index.php'; ?>">AIRYALPS</a>
+            <a class="navbar-brand" href="<?php echo strpos($_SERVER['PHP_SELF'], 'views/') !== false ? '../index.php' : 'index.php'; ?>">
+                <img src="<?php echo strpos($_SERVER['PHP_SELF'], 'views/') !== false ? '../assets/images/airy-alps-logo.svg' : 'assets/images/airy-alps-logo.png'; ?>" alt="AIRYALPS" height="30">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
